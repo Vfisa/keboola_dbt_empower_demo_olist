481bb8ff8f7e1c2b6bf46bb206ebc120ef14d597
VFisa <vfisa.stuff@gmail.com>
2024-02-15 14:54:45 -0800

    product model fixes

--- models/staging/stg_products.sql
+++ models/staging/stg_products.sql
@@ -6,13 +6,13 @@ renamed as (
     select
         "product_id" as product_pk
         ,"product_category_name" as product_category_name
-        ,"product_name_lenght"::integer as product_name_lenght
-        ,"product_description_lenght"::integer as product_description_lenght
-        ,"product_photos_qty"::integer as product_photos_qty
-        ,"product_weight_g"::integer as product_weight_g
-        ,"product_length_cm"::integer as product_length_cm
-        ,"product_height_cm"::integer as product_height_cm
-        ,"product_width_cm"::integer as product_width_cm
+        ,TRY_CAST("product_name_lenght" AS integer) as product_name_lenght
+        ,TRY_CAST("product_description_lenght" AS integer) as product_description_lenght
+        ,TRY_CAST("product_photos_qty" AS integer) as product_photos_qty
+        ,TRY_CAST("product_weight_g" AS integer) as product_weight_g
+        ,TRY_CAST("product_length_cm" AS integer) as product_length_cm
+        ,TRY_CAST("product_height_cm" AS integer) as product_height_cm
+        ,TRY_CAST("product_width_cm" AS integer) as product_width_cm
     from source
 )
 select *
